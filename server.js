a27ea180dc837a9ad426d448c34d88446bb9845d
Pacific <mupacif@gmail.com>
2015-11-10 16:45:21 +0100

    serveur ajout gestion post

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var qs = require('querystring');
 var mysql = require('mysql');
 var http = require('http');
 var url = require('url');
@@ -32,6 +33,26 @@ mySqlClient.query(
   }
 );
 var server = http.createServer(function(req,res){
+    
+function (request, response) {
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            var post = qs.parse(body);
+            console.log(post['login']);
+        });
+    }
+    
 //chargemnt d'un page html
 var page = url.parse(req.url).pathname;
     if(page=="/")
